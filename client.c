33d7287c35c00ca06e16d87658a1a84ab1675b2a
Mateusz Wojtasz <mateuszwojtasz1@gmail.com>
2025-01-20 14:10:39 +0100

    modified thread logic, randomize clients

--- client.c
+++ client.c
@@ -3,53 +3,73 @@
 typedef struct {
     int childAge;
     int hasPampers;
-    int adultAge;
     int targetPool;
-} ChildGuardian;
+} Child;
 // random age generator
 int randRange(int min, int max) { return min + rand() % (max - min + 1); }
-void* childGuardianThread(void* arg) {
-    ChildGuardian* data = (ChildGuardian*)arg;
+// Funkcja wątku dla dziecka
+void* childThread(void* arg) {
+    Child* child = (Child*)arg;
 
     int shmid = getSharedMemory();
     if (shmid == -1) {
-        fprintf(stderr, "Thread: getSharedMemory error.\n");
+        fprintf(stderr, "Thread: error getSharedMemory.\n");
         pthread_exit(NULL);
     }
     SharedMemory* shdata = attachSharedMemory(shmid);
     if (shdata == (void*)-1) {
-        fprintf(stderr, "Thread: attachSharedMemory error.\n");
+        fprintf(stderr, "Thread: error attachSharedMemory.\n");
         pthread_exit(NULL);
     }
+    // test
+    sleep(5);
 
-    printf("[Thread] ending .\n");
+    printf("[Thread] Child ending.\n");
 
     detachSharedMemory(shdata);
 
     pthread_exit(NULL);
 }
+
 int main(int argc, char* argv[]) {
     srand(time(NULL) ^ getpid());
     if (argc < 1) {
         fprintf(stderr, "Wrong arguments: %s\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+    int hasChild = randRange(0, 1);  // 0 - no child, 1 - child
 
-    int age = randRange(1, 70);
-    int poolID;
-    int pampers = 0;
-    if (age < 6) {
-        poolID = CHILD;
-        if (age < 4) {
-            pampers = 1;
+    Child child;
+    pthread_t thread;
+    if (hasChild) {
+        child.childAge = randRange(1, 9);
+        child.hasPampers = (child.childAge <= 3) ? 1 : 0;
+        child.targetPool =
+            (child.childAge <= 5)
+                ? CHILD
+                : RECRE;  // 5 and younger to CHILD, older to recre
+
+        printf("Client: I have a child %d years old. Pampers: %s\n",
+               child.childAge, child.hasPampers ? "yes" : "no");
+
+        if (pthread_create(&thread, NULL, childThread, &child) != 0) {
+            perror("Client: pthread_create");
+            exit(EXIT_FAILURE);
         }
-    } else if (age < 18 && age > 5) {
-        poolID = RECRE;
     } else {
-        poolID = randRange(1, 2);
+        printf("Client without Child.\n");
     }
 
-    printf("Client: Random age: %d years, Pool: %d\n", age, poolID);
+    int age = randRange(18, 70);
+    int poolId;
+
+    if (hasChild && child.childAge <= 3) {
+        poolId = 3;  // need to be with child when is young
+    } else {
+        poolId = randRange(1, 2);
+    }
+
+    printf("Client: Random age: %d years, Pool: %d\n", age, poolId);
 
     int shmid = createSharedMemory();
     if (shmid == -1) {
@@ -62,10 +82,16 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Client: problem with attach shmem.\n");
         exit(EXIT_FAILURE);
     }
-
-    printf("Client: i have shmem! olimpicCount: %d\n", shdata->olimpicCount);
+    printf("Im %d entering the pool \n", getpid());
+    // temporary for check
+    sleep(5);
     if (detachSharedMemory(shdata) == -1) {
         fprintf(stderr, "Client: problem with detach shmem.\n");
     }
-    printf("Hello from client aged %d on pool %d \n", age, poolID);
+    if (hasChild) {
+        pthread_join(thread, NULL);
+        printf("Client: Child ended.\n");
+    }
+    printf("Im %d leaving the pool \n", getpid());
+    return 0;
 }
